115201474cc56d3a21ab23a9dd4bd2a2e04240e7
DennisPierins <dennispierins@gmail.com>
2021-06-09 17:44:34 +0200

    Deleted demo button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,28 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  //useState must always be called within a function (and will call the function again)
-  const [title, setTitle] = useState(props.title);
-  console.log('ExpenseItem evaluated by React')
-
-  //Defining clickHandler (best not to produce function within return statement)
-  const clickHandler = () => {
-    setTitle('Updated!');
-    console.log(title);
-  };
   return (
     <Card className="expense-item">
       {/*props.date needs to be funneled again!*/}
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
